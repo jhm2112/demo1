b6798223f1b472c27e50c423c542c33efd2002d9
jhm2112 <79220026+jhm2112@users.noreply.github.com>
2022-02-03 19:59:21 -0800

    new files and test

---
+++ demoTester.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class demoTester {
+    private Demo demo1;
+    @Test
+    public void multiply1() {
+        demo1 = new Demo();
+        assertEquals(4, demo1.multiply(1,5));
+    }
+}
